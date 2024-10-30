f77e39213a23beefb90c184be9f7e1a173151b23
Timotej Sedláček <timotejs48@gmail.com>
2024-10-30 17:09:41 +0100

    .

---
+++ darker_nav.js
@@ -0,0 +1,15 @@
+myMove = () => {
+    const elem = document.getElementById('navbar');
+    window.addEventListener('scroll', () => {
+        const scrollPosition = document.documentElement.scrollTop;
+
+        if (window.scrollY > 50) {
+            elem.style.background = "#0A1F44";
+            elem.style.transition = "background 0.75s ease-in-out";
+        } else {
+            elem.style.background = "transparent";
+        }
+    })
+}
+
+myMove();
