38b9a22d166abe8f7fb5fb45bff2491f0a26445a
Timotej Sedláček <timotejs48@gmail.com>
2024-10-31 14:05:58 +0100

    .

--- profile.php
+++ profile.php
@@ -12,7 +12,7 @@ $guild_id = '1280088464997482498';
 $role_name = 'N/A';
 $unit_name = 'N/A';
 
-// Mapovanie ID rolí
+// Mapping of role IDs
 $roles_map = [
     '1280093859598041148' => 'Captain',
     '1280093929835986975' => 'Commander',
@@ -28,37 +28,30 @@ $roles_map = [
     '1280111110116347915' => 'Petty Officer First Class',
     '1280111149483823185' => 'Petty Officer Second Class',
     '1280111462743937065' => 'Petty Officer Third Class',
-    '1280111608772694129' => 'Seaman Apprentice',
-    '1280111673444663387' => 'Seaman Recruit',
+    '1280111450985437204' => 'Seaman',
+    '1280111788006981724' => 'Seaman Recruit',
 ];
 
-$units_map = [
-    '1280842675825803286' => 'NSWDG, United States Navy',
-    '1280842681110630410' => 'Phantom Squadron, USAF',
+// Check user's guild roles
+$guild_members_url = "https://discord.com/api/v10/guilds/$guild_id/members/{$user['id']}";
+$guild_members_options = [
+    'http' => [
+        'header' => "Authorization: Bot YOUR_BOT_TOKEN_HERE\r\n", // Zmeňte na svoj bot token
+    ],
 ];
+$guild_members_context = stream_context_create($guild_members_options);
+$guild_members_response = file_get_contents($guild_members_url, false, $guild_members_context);
+$guild_members_data = json_decode($guild_members_response, true);
 
-// Zavoláme Discord API pre informácie o členstve na serveri
-$ch = curl_init("https://discord.com/api/v10/guilds/$guild_id/members/{$user['id']}");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ["Authorization: Bot MTMwMTUwMzc1NTk3OTg1Mzk1Nw.G_0UL7.pYx3sLiq31-Km4i1bYPPoAwWuKqi4w6TAg2iX4"]);
-$response = curl_exec($ch);
-curl_close($ch);
-
-if ($response && ($member_data = json_decode($response, true)) && isset($member_data['roles'])) {
-    // Priradíme rolu podľa ID
-    $roles = $member_data['roles'];
-    $role_names = [];
-    foreach ($roles as $role_id) {
+if (isset($guild_members_data['roles']) && is_array($guild_members_data['roles'])) {
+    foreach ($guild_members_data['roles'] as $role_id) {
         if (isset($roles_map[$role_id])) {
-            $role_names[] = $roles_map[$role_id];
-        }
-        // Priradíme unit na základe ID
-        if (isset($units_map[$role_id])) {
-            $unit_name = $units_map[$role_id];
+            $role_name = $roles_map[$role_id];
+            break;
         }
     }
-    $role_name = implode(', ', $role_names);
 }
+
 ?>
 
 <!doctype html>
@@ -68,8 +61,19 @@ if ($response && ($member_data = json_decode($response, true)) && isset($member_
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./bootstrap/app.css">
     <link rel="shortcut icon" type="image/jpeg" href="./images/jtfs_logo.jpg">
-    <title>Profil používateľa</title>
+    <meta property="og:image" content="./images/jtfs_logo.jpg">
+    <meta property="og:image:type" content="image/jpeg">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="#">
+    <meta property="og:title" content="Joint Task Force Spectre">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <title>Profil - Joint Task Force Spectre</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
 <nav id="navbar" class="navbar navbar-expand-md navbar-dark fixed-top">
@@ -78,7 +82,7 @@ if ($response && ($member_data = json_decode($response, true)) && isset($member_
     </a>
     <div class="container-fluid justify-content-center">
         <ul class="navbar-nav">
-            <li class="nav-item"><a href="index.php" class="nav-link text-light">DOMOV</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-light">DOMOV</a></li>
             <li class="nav-item"><a href="#" class="nav-link text-light">O NÁS</a></li>
             <li class="nav-item"><a href="#" class="nav-link text-light">HOVORIŤ S RECRUITEROM</a></li>
             <li class="nav-item">
@@ -88,21 +92,16 @@ if ($response && ($member_data = json_decode($response, true)) && isset($member_
     </div>
 </nav>
 
-<div>
-
-    <div class="text-over-image">
-        <img src="./images/107410_20241023235653_1.png" style="width: 100%; height: auto;">
-        <div class="text-center text-light">
-            <h1>Vitaj, <?php echo htmlspecialchars($user['username']); ?>!</h1>
-            <img src="https://cdn.discordapp.com/avatars/<?php echo $user['id']; ?>/<?php echo $user['avatar']; ?>.png" class="rounded-img" alt="Avatar" />
-            <p>ID používateľa: <?php echo htmlspecialchars($user['id']); ?></p>
-            <p>Rank: <?php echo htmlspecialchars($role_name); ?></p>
-            <p>Unit: <?php echo htmlspecialchars($unit_name); ?></p>
-        </div>
+<div class="container mt-5 pt-5">
+    <h1 class="text-center">Profil používateľa</h1>
+    <div class="text-center mt-3">
+        <img src="<?= htmlspecialchars($user['avatar']) ? "https://cdn.discordapp.com/avatars/{$user['id']}/{$user['avatar']}.png" : 'https://cdn.discordapp.com/embed/avatars/0.png' ?>" alt="Avatar" class="rounded-circle" width="150" height="150">
     </div>
-
+    <h3 class="text-center mt-3"><?= htmlspecialchars($user['username']) ?>#<?= htmlspecialchars($user['discriminator']) ?></h3>
+    <p class="text-center">Rola: <?= htmlspecialchars($role_name) ?></p>
 </div>
 
 <script src="./darker_nav.js"></script>
+
 </body>
 </html>
