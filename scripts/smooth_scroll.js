c07f564478c9f3145f2be255d237977d5320350f
Timotej Sedláček <timotejs48@gmail.com>
2024-12-03 14:08:58 +0100

    §

--- scripts/smooth_scroll.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("scrollButton").addEventListener("click", function() {
-    const targetElement = document.getElementById("apply");
-    targetElement.scrollIntoView({ behavior: "smooth" });
-});
